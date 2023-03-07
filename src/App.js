4fa3c09ef68653efafad433193374a652f32646c
tkent97 <tristen123abc@gmail.com>
2023-03-06 16:24:18 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navbar />
+        <Route path='/' exact />
+      </Router>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import {Link} from 'react-router-dom';
 const Navbar = () => {
     return (
         <div>
             <nav className='navbar'>
                 <div className="navbar-container">
                     <Link to="/" className="navbar-logo">
-
+                        TRVL <i className='fab fa-typo3'/>
                     </Link>
                 </div>
             </nav>
