db8f45ddf20afd1d4d38b8773541bfd9857a366b
tkent97 <tristen123abc@gmail.com>
2023-03-16 17:37:07 -0700

    Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "../assets/Home.css";
 
 const Home = (props) => {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <>
+            <main>
+                <div className="home-container">
+                    <div className="main-title">
+                        <h2>Popular Vinyls</h2>
+                    </div>
+                    <div className="albumHolder">
+                        <div class="image" className="musicAlbums">
+                            <img src="" alt="" className="albumItems" />
+                            <div class="hover-items">
+                                <h3>View Vinyl</h3>
+                                <div class="icons">
+                                    <NavLink to="/display-vinyl">
+                                        <i class="fa-solid fa-music"></i>
+                                    </NavLink>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="rightcontainer"></div>
+            </main>
+        </>
     );
 };
 
