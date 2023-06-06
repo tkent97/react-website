045ced6a0aed49aa2753693dfb584a9cc05faa97
tkent97 <tristen123abc@gmail.com>
2023-06-06 13:23:35 -0700

    Searchbar added

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,20 @@ import Navbar from './components/navbar';
 import Home from './components/home';
 import LoginPage from './components/loginPage';
 import { Route, Routes } from "react-router-dom";
+import React, { useState } from 'react';
 import SignUp from './components/signUp';
 import VinylDisplay from './components/vinylDisplay';
 
 function App() {
+  const [showHelper, setShowHelper] = useState(false);
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar showHelper={showHelper} setShowHelper={setShowHelper} />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/signup' element={<SignUp />} />
+        <Route path='/' element={<Home onClick={() => setShowHelper(false)} />} />
+        <Route path='/login' element={<LoginPage onClick={() => setShowHelper(false)} />} />
+        <Route path='/signup' element={<SignUp onClick={() => setShowHelper(false)} />} />
         <Route path='/display-vinyl/:id' element={<VinylDisplay />} />
       </Routes>
 
